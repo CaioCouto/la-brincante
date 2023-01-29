a85815ca6306150ae02afd0a99eb40df864a15b8
Caio Couto <coutocaio3@gmail.com>
2023-01-29 11:08:08 -0300

    abstracting formValidation function

--- web/src/utils/index.js
+++ web/src/utils/index.js
@@ -5,6 +5,7 @@ import minutesToHours from "./minutesToHours";
 import getCheckedDays from "./getCheckedDays";
 import closeAlertTimeout from "./closeAlertTimeout";
 import displayClassTimeSpan from "./displayClassTimeSpan";
+import validateForm from "./validateForm";
 
 export {
     weekdays,
@@ -14,4 +15,5 @@ export {
     getCheckedDays,
     closeAlertTimeout,
     displayClassTimeSpan,
+    validateForm,
 };

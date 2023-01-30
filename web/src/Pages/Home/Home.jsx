05971b7918a90e6f8d406878e9e8bad55089b6fb
Caio Couto <coutocaio3@gmail.com>
2023-01-30 07:28:02 -0300

    Adapting Home to multiple classTimes

--- web/src/Pages/Home/Home.jsx
+++ web/src/Pages/Home/Home.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Table } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 import styles from './home.module.css';
 import { Enrollments } from '../../Models';
 import { Alert, Spinner, Card, Divider } from '../../Components';
-import { minutesToHours, weekdays } from '../../utils';
+import { weekdays } from '../../utils';
 
 export default function Home() {
-    const [ chosenDay, setChosenDay ] = useState(new Date().getDay() - 1);
-    const [ loading, setLoading ] = useState({ show: false });
-    const [ alert, setAlert ] = useState({ show: false });
     const [ classes, setClasses ] = useState([]);
+    const [ loading, setLoading ] = useState(false );
+    const [ alert, setAlert ] = useState({ show: false });
+    const [ chosenDay, setChosenDay ] = useState(new Date().getDay());
 
     async function getAllClasses() {
         const alert = {};
         setLoading(true);
         try {
             let response = await Enrollments.getAll();
-            response = response.data.filter(datum => datum.classDays.search(chosenDay.toString()) !== -1);      
+            response = response.data.filter(datum => datum.classDays.search(chosenDay.toString()) !== -1);
             setClasses(response);
         } catch (error) {
             alert.show = true;
@@ -34,7 +34,6 @@ export default function Home() {
         getAllClasses();
     }, [ chosenDay ])
     
-
     return (
         <>
             <Alert
@@ -50,7 +49,6 @@ export default function Home() {
                     {
                         weekdays.map((day, index) => (
                             <option key={ index } value={ index } className="text-capitalize">{ day }</option>
-                            
                             ))
                         }
                 </Form.Select>
@@ -62,8 +60,8 @@ export default function Home() {
                 loading ?
                 <Spinner show={ loading } /> :
                 classes.length === 0 ?
-                <h4>Não há aulas para { weekdays[chosenDay] }.</h4> : 
-                classes.map(data => <Card key={ data.id } data={ data } className={ styles['card'] }/>)
+                <h4>Não há aulas para { weekdays[chosenDay] }.</h4> :
+                classes.map(data => <Card key={ data.id } data={ data } className={ styles['card'] } today={ chosenDay }/>)
                 
             }
         </>

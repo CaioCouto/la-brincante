af224c501b3cb1c1f02d106d428f92c878c18416
Caio Couto <coutocaio3@gmail.com>
2023-01-31 12:11:23 -0300

    Moved Card component to Home, since only it was using that.

--- web/src/Components/index.js
+++ web/src/Components/index.js
@@ -4,12 +4,10 @@ import Modal from "./Modal";
 import Alert from "./Alert";
 import Table from "./Table";
 import Spinner from "./Spinner";
-import Card from "./Card";
 import Divider from "./Divider";
 import TimeInput from "./TimeInput";
 
 export {
-    Card,
     Alert,
     Modal,
     Table,
--- web/src/Pages/Home/Home.jsx
+++ web/src/Pages/Home/Home.jsx
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Card as BsCard } from 'react-bootstrap';
 
 import styles from './home.module.css';
 import { Enrollments } from '../../Models';
-import { Alert, Spinner, Card, Divider } from '../../Components';
-import { weekdays } from '../../utils';
+import { Alert, Spinner, Divider } from '../../Components';
+import { minutesToHours, weekdays } from '../../utils';
 
 export default function Home() {
     const [ classes, setClasses ] = useState([]);
     const [ loading, setLoading ] = useState(false );
     const [ alert, setAlert ] = useState({ show: false });
     const [ chosenDay, setChosenDay ] = useState(new Date().getDay());
+
+    function displayClassTimeSpan(data) {
+        const days = data.classDays.split(',');
+        const times = data.classTime.split(',');
+        const durations = data.duration.split(',');
+        const todayIndex = days.indexOf(chosenDay.toString());
+        const todayClassTime = days.length === times.length ? parseInt(times[todayIndex]) : parseInt(times[0]);
+        const todayClassDuration = days.length === times.length ? parseInt(durations[todayIndex]) : parseInt(durations[0]);        
+        return minutesToHours(todayClassTime) + ' - ' + minutesToHours(todayClassTime + todayClassDuration);
+    }
 
     async function getAllClasses() {
         const alert = {};
@@ -61,7 +71,17 @@ export default function Home() {
                 <Spinner show={ loading } /> :
                 classes.length === 0 ?
                 <h4>Não há aulas para { weekdays[chosenDay] }.</h4> :
-                classes.map(data => <Card key={ data.id } data={ data } className={ styles['card'] } today={ chosenDay }/>)
+                classes.map(data => 
+                    <BsCard key={ data.id } className={ `container-sm ${styles['card']}` }>
+                        <BsCard.Body className="d-flex justify-content-between align-items-center">
+                            <div>
+                                <BsCard.Title>{ data.students.name }</BsCard.Title>
+                                <BsCard.Text>{ data.course.name } ({ data.isOnline ? 'Online' : 'Presencial' })</BsCard.Text>
+                            </div>
+                            <BsCard.Subtitle className="text-muted">{ displayClassTimeSpan(data) }</BsCard.Subtitle>
+                        </BsCard.Body>
+                    </BsCard>
+                )
                 
             }
         </>

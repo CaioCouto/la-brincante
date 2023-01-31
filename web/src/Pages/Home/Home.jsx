df869755f30ef098f48ec858c4a8c1cc0eacdc98
Caio Couto <coutocaio3@gmail.com>
2023-01-31 17:20:24 -0300

    Sorting classes by classTime

--- web/src/Pages/Home/Home.jsx
+++ web/src/Pages/Home/Home.jsx
@@ -6,17 +6,35 @@ import { Enrollments } from '../../Models';
 import { Alert, Spinner, Divider } from '../../Components';
 import { minutesToHours, weekdays } from '../../utils';
 
+function sortClassesByClassTime(classes, today) {
+    /**
+     * Via Passagem de referência, o array
+     * 'classes' está send ordenado de acordo
+     * com o classTime do dia selecionado (today)
+     */
+    classes.sort((a,b) => {
+        const [ daysA, daysB ] = [ a.classDays.split(','), b.classDays.split(',')];
+        const [ timesA, timesB ] = [ a.classTime.split(','), b.classTime.split(',')];
+        const [ todayIndexA, todayIndexB] = [ daysA.indexOf(today), daysB.indexOf(today) ];
+        if(timesA.length === timesB.length) 
+            return parseInt(timesA[todayIndexA]) - parseInt(timesB[todayIndexB]);
+        else if(timesA.length > timesB.length) 
+            return parseInt(timesA[todayIndexA]) - parseInt(timesB[0]);
+        return parseInt(timesA[0]) - parseInt(timesB[todayIndexB]);
+    });
+}
+
 export default function Home() {
     const [ classes, setClasses ] = useState([]);
     const [ loading, setLoading ] = useState(false );
     const [ alert, setAlert ] = useState({ show: false });
-    const [ chosenDay, setChosenDay ] = useState(new Date().getDay());
+    const [ chosenDay, setChosenDay ] = useState(new Date().getDay().toString());
 
     function displayClassTimeSpan(data) {
         const days = data.classDays.split(',');
         const times = data.classTime.split(',');
         const durations = data.duration.split(',');
-        const todayIndex = days.indexOf(chosenDay.toString());
+        const todayIndex = days.indexOf(chosenDay);
         const todayClassTime = days.length === times.length ? parseInt(times[todayIndex]) : parseInt(times[0]);
         const todayClassDuration = days.length === times.length ? parseInt(durations[todayIndex]) : parseInt(durations[0]);        
         return minutesToHours(todayClassTime) + ' - ' + minutesToHours(todayClassTime + todayClassDuration);
@@ -26,9 +44,9 @@ export default function Home() {
         const alert = {};
         setLoading(true);
         try {
-            let response = await Enrollments.getAll();
-            response = response.data.filter(datum => datum.classDays.search(chosenDay.toString()) !== -1);
-            setClasses(response);
+            let response1 = await Enrollments.getByClassDay(chosenDay);
+            sortClassesByClassTime(response1.data, chosenDay);
+            setClasses(response1.data);
         } catch (error) {
             alert.show = true;
             alert.variant = 'danger';

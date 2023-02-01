dfb10eb2991322263230487c754660b6bfdbeea2
Caio Couto <coutocaio3@gmail.com>
2023-02-01 08:39:29 -0300

    Added a Templates Controller

--- backend/src/Controllers/index.ts
+++ backend/src/Controllers/index.ts
@@ -1,9 +1,11 @@
 import CoursesController from "./CoursesController";
 import StudentsController from "./StudentsController";
 import EnrollmentsController from "./EnrollmentsController";
+import TemplatesController from "./TemplatesController";
 
 export {
     CoursesController,
     StudentsController,
-    EnrollmentsController
+    EnrollmentsController,
+    TemplatesController
 }

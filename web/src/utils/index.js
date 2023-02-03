ec637d2b453e28ca86ee93bef4c5c07924382992
Caio Couto <coutocaio3@gmail.com>
2023-02-03 09:53:49 -0300

    Exported the new functoins & organized everything

--- web/src/utils/index.js
+++ web/src/utils/index.js
@@ -1,19 +1,25 @@
 import weekdays from "./weekdays";
-import classDaysCheckBoxes from "./classDaysCheckBoxes";
+import capitalize from "./capitalize";
+import generatePDF from "./generatePDF";
+import validateForm from "./validateForm";
 import hoursToMinutes from "./hoursToMinutes";
 import minutesToHours from "./minutesToHours";
 import getCheckedDays from "./getCheckedDays";
 import closeAlertTimeout from "./closeAlertTimeout";
-import validateForm from "./validateForm";
-import capitalize from "./capitalize";
+import classDaysCheckBoxes from "./classDaysCheckBoxes";
+import displayClassTimeSpan from "./displayClassTimeSpan";
+import sortClassesByClassTime from "./sortClassesByClassTime";
 
 export {
     weekdays,
+    validateForm,
     classDaysCheckBoxes,
+    capitalize,
+    generatePDF,
     hoursToMinutes,
     minutesToHours,
     getCheckedDays,
     closeAlertTimeout,
-    validateForm,
-    capitalize
+    displayClassTimeSpan,
+    sortClassesByClassTime,
 };

9a2a27855c79a3f53f227a49dde5edca56029507
Caio Couto <coutocaio3@gmail.com>
2023-02-03 09:53:31 -0300

    Implementing a "PDF generator" button

--- web/src/Pages/Home/Home.jsx
+++ web/src/Pages/Home/Home.jsx
@@ -1,52 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Card as BsCard } from 'react-bootstrap';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 import styles from './home.module.css';
 import { Enrollments } from '../../Models';
-import { Alert, Spinner, Divider } from '../../Components';
-import { minutesToHours, weekdays } from '../../utils';
+import { Alert, Spinner, Divider, Button, PDFTemplate } from '../../Components';
+import { weekdays, sortClassesByClassTime, displayClassTimeSpan, generatePDF } from '../../utils';
 
-function sortClassesByClassTime(classes, today) {
+function groupEnrollmentsByClassDay(data) {
     /**
-     * Via Passagem de referência, o array
-     * 'classes' está send ordenado de acordo
-     * com o classTime do dia selecionado (today)
+     * @param Array<enrollment>: data
+     * @returns Array<Object{day:int, classes:enrollment[]}>
+     * 
+     * Retorna um array de objetos, os quais
+     * contém dois atributos: "day", que guarda
+     * índice do dia da semana; e "classes", um
+     * array contendo todas as aulas daquele dia
+     * em específico.
      */
-    classes.sort((a,b) => {
-        const [ daysA, daysB ] = [ a.classDays.split(','), b.classDays.split(',')];
-        const [ timesA, timesB ] = [ a.classTime.split(','), b.classTime.split(',')];
-        const [ todayIndexA, todayIndexB] = [ daysA.indexOf(today), daysB.indexOf(today) ];
-        if(timesA.length === timesB.length) 
-            return parseInt(timesA[todayIndexA]) - parseInt(timesB[todayIndexB]);
-        else if(timesA.length > timesB.length) 
-            return parseInt(timesA[todayIndexA]) - parseInt(timesB[0]);
-        return parseInt(timesA[0]) - parseInt(timesB[todayIndexB]);
+    return weekdays.map((_, index) => {
+        const classes = data.filter(d => d.classDays.indexOf(index.toString()) !== -1);
+        if (classes.length > 1) { sortClassesByClassTime(classes, index.toString()) };
+        return { day: index, classes: classes };
     });
 }
 
+function ClassCards({ enrollments, today }) {
+    return (
+        <>
+        {
+            enrollments[today].classes.map(data => 
+                <BsCard key={ data.id } className={ `container-sm ${styles['card']}` }>
+                    <BsCard.Body className="d-flex justify-content-between align-items-center">
+                        <div>
+                            <BsCard.Title>{ data.students.name }</BsCard.Title>
+                            <BsCard.Text>{ data.course.name } ({ data.isOnline ? 'Online' : 'Presencial' })</BsCard.Text>
+                        </div>
+                        <BsCard.Subtitle className="text-muted">{ displayClassTimeSpan(data, today) }</BsCard.Subtitle>
+                    </BsCard.Body>
+                </BsCard>
+            )
+        }
+        </>
+    );
+}
+
 export default function Home() {
-    const [ classes, setClasses ] = useState([]);
-    const [ loading, setLoading ] = useState(false );
+    const [ enrollments, setEnrollments ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
     const [ alert, setAlert ] = useState({ show: false });
     const [ chosenDay, setChosenDay ] = useState(new Date().getDay().toString());
-
-    function displayClassTimeSpan(data) {
-        const days = data.classDays.split(',');
-        const times = data.classTime.split(',');
-        const durations = data.duration.split(',');
-        const todayIndex = days.indexOf(chosenDay);
-        const todayClassTime = days.length === times.length ? parseInt(times[todayIndex]) : parseInt(times[0]);
-        const todayClassDuration = days.length === times.length ? parseInt(durations[todayIndex]) : parseInt(durations[0]);        
-        return minutesToHours(todayClassTime) + ' - ' + minutesToHours(todayClassTime + todayClassDuration);
-    }
 
     async function getAllClasses() {
         const alert = {};
         setLoading(true);
         try {
-            let response1 = await Enrollments.getByClassDay(chosenDay);
-            sortClassesByClassTime(response1.data, chosenDay);
-            setClasses(response1.data);
+            const responseEnrollments = await Enrollments.getAll();
+            const groupedEnrollments = groupEnrollmentsByClassDay(responseEnrollments.data);
+            setEnrollments(groupedEnrollments);
         } catch (error) {
             alert.show = true;
             alert.variant = 'danger';
@@ -58,10 +69,14 @@ export default function Home() {
         }
     }
 
-    useEffect(() => {
-        getAllClasses();
-    }, [ chosenDay ])
-    
+    async function generateFullTimeTable() {
+        setLoading(true)
+        await generatePDF(renderToStaticMarkup(<PDFTemplate enrollments={ enrollments }/>));
+        setLoading(false);
+    }
+
+    useEffect(() => { getAllClasses(); }, []);
+
     return (
         <>
             <Alert
@@ -72,35 +87,37 @@ export default function Home() {
             <h1 className={ styles['title'] }>Aulas do dia</h1>
 
             <Form>
-                <Form.Label as={"h3"}>Lista de todas as aulas para</Form.Label>
-                <Form.Select value={ chosenDay.toString() } onChange={ (e) => setChosenDay(e.target.value) }>
-                    {
-                        weekdays.map((day, index) => (
-                            <option key={ index } value={ index } className="text-capitalize">{ day }</option>
-                            ))
-                        }
-                </Form.Select>
+                <Form.Group as="section">
+                    <article className='d-flex justify-content-between align-items-center mb-2'>
+                        <Form.Label as={"h3"} className='m-0'>Lista de todas as aulas para</Form.Label>
+                        <div className='d-flex gap-2'>
+                            <Spinner show={ loading }/>
+                            <Button
+                                label="Ver horário completo"
+                                variant="info"
+                                onClickFn={ () => generateFullTimeTable()}
+                            />
+                        </div>
+                    </article>
+                    <Form.Select value={ chosenDay.toString() } onChange={ (e) => setChosenDay(e.target.value) }>
+                        {
+                            weekdays.map((day, index) => (
+                                <option key={ index } value={ index } className="text-capitalize">{ day }</option>
+                                ))
+                            }
+                    </Form.Select>
+                </Form.Group>
             </Form>
 
             <Divider/>
 
             {
-                loading ?
-                <Spinner show={ loading } /> :
-                classes.length === 0 ?
+                (!enrollments[chosenDay] || enrollments[chosenDay].classes.length === 0) ?
                 <h4>Não há aulas para { weekdays[chosenDay] }.</h4> :
-                classes.map(data => 
-                    <BsCard key={ data.id } className={ `container-sm ${styles['card']}` }>
-                        <BsCard.Body className="d-flex justify-content-between align-items-center">
-                            <div>
-                                <BsCard.Title>{ data.students.name }</BsCard.Title>
-                                <BsCard.Text>{ data.course.name } ({ data.isOnline ? 'Online' : 'Presencial' })</BsCard.Text>
-                            </div>
-                            <BsCard.Subtitle className="text-muted">{ displayClassTimeSpan(data) }</BsCard.Subtitle>
-                        </BsCard.Body>
-                    </BsCard>
-                )
-                
+                <ClassCards 
+                    enrollments={ enrollments }
+                    today={ chosenDay }
+                />               
             }
         </>
     );

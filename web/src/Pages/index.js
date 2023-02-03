2f83062fee225addc7e2069bd78777c1da8fcc52
Caio Couto <coutocaio3@gmail.com>
2023-02-03 09:52:24 -0300

    Organized imports

--- web/src/Pages/index.js
+++ web/src/Pages/index.js
@@ -1,17 +1,17 @@
 import Home from "./Home";
+import Courses from "./Courses";
 import Students from "./Students";
 import StudentDetails from "./StudentDetails";
-import Courses from "./Courses";
 import Enrollments from "./Enrollments";
 import EnrollmentDetails from "./EnrollmentDetails";
 import RegisterEnrollment from "./RegisterEnrollment";
 
 export {
     Home,
-    Students,
     Courses,
+    Students,
+    StudentDetails,
     Enrollments,
     EnrollmentDetails,
-    StudentDetails,
-    RegisterEnrollment
+    RegisterEnrollment,
 };
